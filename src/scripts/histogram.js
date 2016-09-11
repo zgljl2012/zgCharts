6da7b67a7219732d2f3e3062e2b883553dafb1de
zgljl2012 <2693491512@qq.com>
2016-09-12 00:13:39 +0800

    直方图添加比例尺以及标签

--- src/scripts/histogram.js
+++ src/scripts/histogram.js
@@ -7,26 +7,51 @@ import { px2num } from "./tools.js"
 var htm = function(dataset) {
 	var zg = zgCharts,
 		svg = zg.svg,
+		padding_left = 15,
+		padding_top = 20,
 		w = svg.style("width"),
 		h = svg.style("height");
 	h = px2num(h)
 	w = px2num(w)
 	console.log(dataset)
+	var xScale = d3.scaleLinear()
+		.domain([0, dataset.length])
+		.range([0, w]);
+	var yScale = d3.scaleLinear()
+		.domain([0, d3.max(dataset, function(d){ return d; })])
+		.range([0, h-padding_top])
+	svg.attr("class", "histogram")
 	svg.selectAll("rect").data(dataset).enter()
 		.append("rect")
 		.attr("width", function(d, i){
-			return 10;
+			return w/dataset.length - padding_left;
 		})
 		.attr("height", function(d){
-			return d*20;
+			return yScale(d);
 		})
-		.attr("fill", "#00ff00")
 		.attr("x", function(d, i) {
-			return i*20;
+			return xScale(i);
 		})
 		.attr("y", function(d, i) {
-			return h - d*20;
-		});
+			return h - yScale(d);
+		})
+	zgCharts.showLabel = function() {
+		svg.selectAll("text")
+			.data(dataset)
+			.enter()
+			.append("text")
+			.text(function(d){
+				return d;
+			})
+			.attr("x", function(d, i){
+				return parseFloat(xScale(i))+parseFloat((w/dataset.length - padding_left)/3);
+			})
+			.attr("y", function(d, i) {
+				return h - yScale(d) - 2;
+			})
+		return zgCharts;
+	}
+	return zgCharts;
 };
 
 export
